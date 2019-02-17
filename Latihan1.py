39dcd6a6f9c6b4e0973b15e1447d03e69d9d4d42
cyndi99 <cyndi.b99@mhs.pelitabangsa.ac.id>
2019-02-17 21:37:51 +0700

    first commit

---
+++ Latihan1.py
@@ -0,0 +1,14 @@
+print ('******************************************************')
+print ('*PROGRAM UNTUK MEMENTUKAN BILANGAN ACAK YANG LEBIH KECIL DARI 0.5*')
+print ('*Nama	: Cyndia Bella Ria Ilhami*')
+print ('*NIM	: 3118100888*')
+print ('*Kelas	: TI.18.A.1*')
+print ('******************************************************')
+
+import random
+jumlah =int(input("masukkan jumlah n: "))
+print ()
+for i in range (jumlah):
+	i=random.uniform(0.0,0.5)
+	print(i)
+print ('========= S E L E S A I ========')
